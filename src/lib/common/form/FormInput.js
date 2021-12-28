aa4eacaf606f7c737978ac940fbd4b2acf677a5a
bablu.gaur@pentair.com <bablu.gaur@pentair.com>
2021-12-28 17:58:32 +0530

    form grp and select work

--- src/lib/common/form/FormInput.js
+++ src/lib/common/form/FormInput.js
@@ -2,25 +2,43 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-export default function FormLabel({ labelText, styleClasses, otherProps }) {
+export default function FormInput({
+  size, type, placeholder,
+  name, onChange, value, isInvalid, dataTestId, otherProps,
+}) {
   return (
-    <Form.Label className={styleClasses} {...otherProps}>
-      {labelText}
-    </Form.Label>
+    <Form.Control
+      size={size}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      isInvalid={isInvalid}
+      onChange={onChange}
+      value={value}
+      data-testid={dataTestId}
+      {...otherProps}
+    />
   );
 }
 
-FormLabel.propTypes = {
-  styleClasses: PropTypes.string,
-  labelText: PropTypes.oneOfType([
-    PropTypes.elementType,
-    PropTypes.string,
-  ]),
+FormInput.propTypes = {
+  size: PropTypes.string,
+  type: PropTypes.string,
+  isInvalid: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
   otherProps: PropTypes.shape({}),
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  dataTestId: PropTypes.string,
 };
 
-FormLabel.defaultProps = {
-  styleClasses: 'form-label-bold',
-  labelText: '',
+FormInput.defaultProps = {
+  size: 'lg',
+  type: 'text',
+  isInvalid: false,
+  placeholder: '',
   otherProps: {},
+  value: '',
+  dataTestId: 'form-input',
 };
